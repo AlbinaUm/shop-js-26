8aeb68a9d479d2e33a1f553cbf8663335f762c81
Umarova Albina <87119449+AlbinaUm@users.noreply.github.com>
2025-04-18 22:49:45 +0600

    Update config.ts

--- api/config.ts
+++ api/config.ts
@@ -16,7 +16,7 @@ const rootPath = __dirname;
 const corsWhiteList = [
     'http://localhost:5173',
     'http://localhost:5183',
-    'http://157.230.115.76:5183',
+    'http://159.223.29.16:5183',
     'http://104.248.20.243:5183'
 ];
 
